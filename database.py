3cf30df4d74ccf57aefbddff0cd0c8decd99d6e5
Vanessa-Ace <v.nishimwe@alustudent.com>
2025-11-12 14:23:27 -0800

    Created database

---
+++ database.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import mysql.connector
+
+class Database:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "",
+            database = "Online_Polling_System"
+        )
+        self.cursor = self.conn.cursor(dictionary= True)
+
+    def execute(self, query, values=None):
+        self.cursor.execute(query, values or ())
+        self.conn.commit()
+
+    def fetch(self, query, values=None):
+        self.cursor.execute(query, values or ())
+        return self.cursor.fetchall()
+    
+    def close(self):
+        self.conn.close()
