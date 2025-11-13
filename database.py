d42e3b285eeba98dc46f7f66a1ce9549e497cff1
Vanessa-Ace <v.nishimwe@alustudent.com>
2025-11-13 02:30:47 -0800

    Database connector

--- database.py
+++ database.py
@@ -4,15 +4,15 @@ import pymysql as mysql
 class Database:
     def __init__(self):
         self.conn = mysql.connector.connect(
-            host = "localhost",
+            host = "mysql-3755b811-alustudent-042b.f.aivencloud.com",
             user = "root",
-            password = "",
+            password = "yourpassword",
             database = "Online_Polling_System"
         )
         self.cursor = self.conn.cursor(dictionary= True)
 
     def execute(self, query, values=None):
-        self.cursor.execute(query, values or ())
+        self.cursor.execute(query, values or ())                                                                                    
         self.conn.commit()
 
     def fetch(self, query, values=None):
